2a15fd41997f4cbae15e5b07d076672dfd79ae90
Marcus <156995121+marcus-ao@users.noreply.github.com>
2025-03-07 16:47:10 +0800

    The commit of initial edition that disable relevent rules.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,18 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // 关闭未使用变量检查，解决 "Image"、"router"、"theme" 未使用的错误
+      "@typescript-eslint/no-unused-vars": "off",
+      // 关闭对 any 类型的检查，解决 Unexpected any 的错误
+      "@typescript-eslint/no-explicit-any": "off",
+      // 关闭非转义字符检查，解决不能转义单引号的错误
+      "react/no-unescaped-entities": "off",
+      // 关闭 <img> 标签使用检查，解决关于使用 <img> 提示的警告
+      "@next/next/no-img-element": "off"
+    }
+  }
 ];
 
 export default eslintConfig;
